e0a4ed63027faca5f154a4b95df02634e577ac54
Rae Knowler <bellisk@users.noreply.github.com>
2024-10-26 17:26:49 +0200

    Use external code url that GreasyFork accepts
    JSDelivr GitHub urls need to contain the commit of the code, not the version.

--- Improve Chinese text on Firefox-2024-10-26.user.js
+++ Improve Chinese text on Firefox-2024-10-26.user.js
@@ -1,22 +1,24 @@
 // ==UserScript==
 // @name         Improve Chinese text on Firefox
 // @namespace    http://tampermonkey.net/
-// @version      2024-10-26
+// @version      2024-10-26-04
 // @description  Change lang="zh" attribute to lang="zh-cn" so Firefox can style it correctly
 // @author       bellisk
 // @match        http://*/*
 // @match        https://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @require      https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.3/waitForKeyElements.js
+// @require      https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@a03933c5e42343b434c7800eb2777575342d8287/waitForKeyElements.js
+// @license      MIT
 // @grant        none
 // @run-at       document-idle
+// @downloadURL https://update.greasyfork.org/scripts/514177/Improve%20Chinese%20text%20on%20Firefox.user.js
+// @updateURL https://update.greasyfork.org/scripts/514177/Improve%20Chinese%20text%20on%20Firefox.meta.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     waitForKeyElements("*:lang(zh)", (element) => {
-        console.log(element);
         element.lang = "zh-cn";
     })
 })();
