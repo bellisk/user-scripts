07345763387642d3036c96807526047799572d86
Rae Knowler <bellisk@users.noreply.github.com>
2025-02-02 19:26:27 +0100

    Refer to works, not fics; hide work summary on single-work page

--- ao3 warn for fics with keywords-2025-02-01.user.js
+++ ao3 warn for fics with keywords-2025-02-01.user.js
@@ -1,10 +1,8 @@
-/*jshint esversion: 8 */
-
 // ==UserScript==
-// @name         ao3 warn for fics with keywords
+// @name         ao3 warn for works with keywords
 // @namespace    http://tampermonkey.net/
 // @version      2025-02-02
-// @description  Adds a warning box on fics that contain certain keywords on AO3
+// @description  Adds a warning box on works that contain certain keywords on AO3
 // @author       bellisk
 // @license      MIT
 // @include      https://archiveofourown.org/*
@@ -20,14 +18,14 @@
 
 var dropdown = document.createElement('li');
 dropdown.innerHTML = `
-    <a class="dropdown-toggle" href="/menu/hide-keywords" data-toggle="dropdown" data-target="#">Hide fics with keywords</a>
+    <a class="dropdown-toggle" href="/menu/hide-keywords" data-toggle="dropdown" data-target="#">Hide works with keywords</a>
     <ul class="menu dropdown-menu" role="menu">
-        <li id="addKeyword" role="menu-item"><a href="#">Add keyword to hide fics for</a></li>
         <li id="clearLast" role="menu-item"><a href="#">Remove last-added keyword</a></li>
         <li id="clearAll" role="menu-item"><a href="#">Remove all keywords</a></li>
+        <li id="addKeyword" role="menu-item"><a href="#">Add keyword to hide works for</a></li>
     </ul>
 `;
-dropdown.className = 'dropdown hideKeywordFics';
+dropdown.className = 'dropdown hideKeywordWorks';
 var primaryNav = document.getElementsByClassName('primary navigation actions')[0];
 primaryNav.appendChild(dropdown);
 
@@ -59,7 +57,7 @@ async function clearLast() {
     location.reload();
 }
 
-// filter and hide fics
+// filter and hide works
 
 const warningBoxCss = `
 .warningBox {
@@ -73,37 +71,37 @@ const warningBoxCss = `
   text-align: center;
 }`
 
-function toggleFicBlurbDisplay(index) {
+function toggleWorkBlurbDisplay(index) {
     const blurbs = document.querySelectorAll('li.blurb');
     const warningButton = document.getElementById(`warningButton${index}`);
     if (blurbs[index].getElementsByClassName('header')[0].style.display === 'none') {
         for (let k=0; k<blurbs[index].children.length; k++) {
             blurbs[index].children[k].style.display = 'block';
         }
-        warningButton.textContent = 'Hide the fic summary';
+        warningButton.textContent = 'Hide the work summary';
     } else {
         for (let k=0; k<blurbs[index].children.length; k++) {
             if (blurbs[index].children[k].className !== 'warningBox') {
                 blurbs[index].children[k].style.display = 'none';
             }
         }
-        warningButton.textContent = 'Display the fic summary';
+        warningButton.textContent = 'Display the work summary';
     }
 }
 
-async function filterListOfFics() {
-    const fics = document.querySelectorAll('li.blurb');
-    if (fics.length == 0) {
+async function filterListOfWorks() {
+    const works = document.querySelectorAll('li.blurb');
+    if (works.length == 0) {
         return;
     }
     const keywordsToHide = await GM_listValues();
-    for (let j=0; j<fics.length; j++) {
+    for (let j=0; j<works.length; j++) {
         const foundKeywords = [];
         for (let k=0; k<keywordsToHide.length; k++) {
             if (keywordsToHide[k] === 'last') {
                 continue;
             }
-            if (fics[j].innerText.toLowerCase().includes(GM_getValue(keywordsToHide[k]))) {
+            if (works[j].innerText.toLowerCase().includes(GM_getValue(keywordsToHide[k]))) {
                 foundKeywords.push(GM_getValue(keywordsToHide[k]));
             }
         }
@@ -112,55 +110,60 @@ async function filterListOfFics() {
             warning.id = `warningBox${j}`;
             warning.className = "warningBox";
             warning.innerHTML = `
-                <h3>Blocked keyword(s) found in this fic summary</h3>
+                <h3>Blocked keyword(s) found in this work summary</h3>
                 <details>
                     <summary>Click to see the keywords found.</summary>
                     <p>${foundKeywords}</p>
                 </details>
-                <button id="warningButton${j}">Display the fic summary</button>
+                <button id="warningButton${j}">Display the work summary</button>
             `;
             GM_addStyle(warningBoxCss);
-            for (let k=0; k<fics[j].children.length; k++) {
-                fics[j].children[k].style.display = 'none';
+            for (let k=0; k<works[j].children.length; k++) {
+                works[j].children[k].style.display = 'none';
             }
-            fics[j].prepend(warning);
-            document.getElementById(`warningButton${j}`).onclick = function() {toggleFicBlurbDisplay(j);};
+            works[j].prepend(warning);
+            document.getElementById(`warningButton${j}`).onclick = function() {toggleWorkBlurbDisplay(j);};
         }
     }
 }
 
-function toggleFicDisplay() {
+function toggleWorkDisplay() {
     const chapters = document.getElementById('chapters');
+    const summary = document.getElementsByClassName('summary')[0];
     const warningButton = document.getElementById('warningButton');
     if (chapters.style.display === 'none') {
         chapters.style.display = 'block';
+        summary.style.display = 'block';
         const notes = document.getElementsByClassName('notes');
         for (let k=0; k<notes.length; k++) {
             notes[k].style.display = 'block';
         }
-        warningButton.textContent = 'Hide the fic';
+        warningButton.textContent = 'Hide the work';
     } else {
         chapters.style.display = 'none';
+        summary.style.display = 'none';
         const notes = document.getElementsByClassName('notes');
         for (let k=0; k<notes.length; k++) {
             notes[k].style.display = 'none';
         }
-        warningButton.textContent = 'Display the fic';
+        warningButton.textContent = 'Display the work';
     }
 }
 
-async function addWarningBoxForSingleFicPage() {
+async function addWarningBoxForSingleWorkPage() {
     const chapters = document.getElementById('chapters');
     if (chapters === null) {
         return;
     }
+    const summary = document.getElementsByClassName('summary')[0];
     const keywordsToHide = await GM_listValues();
     const foundKeywords = [];
     for (let j=0; j<keywordsToHide.length; j++) {
         if (keywordsToHide[j] == 'last') {
             continue;
         }
-        if (chapters.innerText.toLowerCase().includes(GM_getValue(keywordsToHide[j]))) {
+        if (chapters.innerText.toLowerCase().includes(GM_getValue(keywordsToHide[j])) ||
+           summary.innerText.toLowerCase().includes(GM_getValue(keywordsToHide[j]))) {
             foundKeywords.push(GM_getValue(keywordsToHide[j]));
         }
     }
@@ -169,30 +172,31 @@ async function addWarningBoxForSingleFicPage() {
         warning.id = "warningBox";
         warning.className = "warningBox";
         warning.innerHTML = `
-            <h3>Blocked keyword(s) found in this fic</h3>
+            <h3>Blocked keyword(s) found in this work</h3>
             <details>
                 <summary>Click to see the keywords found.</summary>
                 <p>${foundKeywords}</p>
             </details>
-            <button id="warningButton">Display the fic</button>
+            <button id="warningButton">Display the work</button>
         `;
         GM_addStyle(warningBoxCss);
         const summary = document.getElementsByClassName('summary')[0];
         summary.parentNode.insertBefore(warning, summary);
 
         chapters.style.display = 'none';
+        summary.style.display = 'none';
         const notes = document.getElementsByClassName('notes');
         for (let k=0; k<notes.length; k++) {
             notes[k].style.display = 'none';
         }
-        document.getElementById('warningButton').onclick = function() {toggleFicDisplay();};
+        document.getElementById('warningButton').onclick = function() {toggleWorkDisplay();};
     }
 }
 
 // run
 
-document.getElementById('addKeyword').onclick = function() {addKeyword();};
 document.getElementById('clearLast').onclick = function() {clearLast();};
 document.getElementById('clearAll').onclick = function() {clearAll();};
-filterListOfFics();
-addWarningBoxForSingleFicPage();
+document.getElementById('addKeyword').onclick = function() {addKeyword();};
+filterListOfWorks();
+addWarningBoxForSingleWorkPage();
